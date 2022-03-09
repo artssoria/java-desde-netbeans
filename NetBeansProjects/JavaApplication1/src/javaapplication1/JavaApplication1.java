88c995c420626ae486e2915fcf6214b1e2791341
artssoria <arts.soria9@gmail.com>
2022-03-08 22:21:31 -0300

    primer commit de java en netbeans

---
+++ NetBeansProjects/JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author arturo
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        char letra;
+        
+        System.out.println("digite una cadena: ");
+        letra = entrada.nextLine().charAt(0);
+        
+        System.out.println("la cadena: " + letra);
+       
+    }
+    
+}
