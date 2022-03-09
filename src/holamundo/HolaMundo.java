bb02fad5ba00edb2e10df468a25ded919ef51ae9
artssoria <arts.soria9@gmail.com>
2022-03-09 02:19:55 -0300

    format

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -18,19 +18,20 @@ public class HolaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       String cadena;
-       int entero;
-       char letra;
-       double decimal;
-               cadena = JOptionPane.showInputDialog("digite una cadena");
-               entero = Integer.parseInt(JOptionPane.showInputDialog("Digite un entero: "));
-               letra = JOptionPane.showInputDialog("Digite un caracter: ").charAt(0);
-               decimal = Double.parseDouble(JOptionPane.showInputDialog("Digite un decinal: "));
-               
-               JOptionPane.showMessageDialog(null, "La cadena es: " + cadena);      JOptionPane.showMessageDialog(null, "El entero  es: " + entero);
-               JOptionPane.showMessageDialog(null, "El caracter es: " + letra);
-               JOptionPane.showMessageDialog(null, "El decimal es: " + decimal);
-               
+        String cadena;
+        int entero;
+        char letra;
+        double decimal;
+        cadena = JOptionPane.showInputDialog("digite una cadena");
+        entero = Integer.parseInt(JOptionPane.showInputDialog("Digite un entero: "));
+        letra = JOptionPane.showInputDialog("Digite un caracter: ").charAt(0);
+        decimal = Double.parseDouble(JOptionPane.showInputDialog("Digite un decinal: "));
+
+        JOptionPane.showMessageDialog(null, "La cadena es: " + cadena);
+        JOptionPane.showMessageDialog(null, "El entero  es: " + entero);
+        JOptionPane.showMessageDialog(null, "El caracter es: " + letra);
+        JOptionPane.showMessageDialog(null, "El decimal es: " + decimal);
+
     }
-    
+
 }
