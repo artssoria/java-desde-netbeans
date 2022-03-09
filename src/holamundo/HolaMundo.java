da04e534774941685fcfe34ca67449fdcd0fec43
artssoria <arts.soria9@gmail.com>
2022-03-09 02:18:25 -0300

    datos del JOpane

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -27,6 +27,9 @@ public class HolaMundo {
                letra = JOptionPane.showInputDialog("Digite un caracter: ").charAt(0);
                decimal = Double.parseDouble(JOptionPane.showInputDialog("Digite un decinal: "));
                
+               JOptionPane.showMessageDialog(null, "La cadena es: " + cadena);      JOptionPane.showMessageDialog(null, "El entero  es: " + entero);
+               JOptionPane.showMessageDialog(null, "El caracter es: " + letra);
+               JOptionPane.showMessageDialog(null, "El decimal es: " + decimal);
                
     }
     
