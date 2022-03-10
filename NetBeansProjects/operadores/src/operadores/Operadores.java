9adcad0616c7d85c4c861f313e2eccba18e3e25a
artssoria <arts.soria9@gmail.com>
2022-03-10 00:23:03 -0300

    operadores

---
+++ NetBeansProjects/operadores/src/operadores/Operadores.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadores;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author arturo
+ */
+public class Operadores {
+
+    public static void main(String[] args) {
+       int numero = 10;
+       
+       
+        System.out.println(numero);
+        
+    }
+    
+}
